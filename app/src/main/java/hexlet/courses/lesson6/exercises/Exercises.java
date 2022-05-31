7db14018db4455ed419bb577f3faba818d9384db
dmartynov <denis.martynov.93@mail.ru>
2022-05-31 17:24:41 +0300

    add buildSnailPath method, exercise 1.6.9

--- app/src/main/java/hexlet/courses/lesson6/exercises/Exercises.java
+++ app/src/main/java/hexlet/courses/lesson6/exercises/Exercises.java
@@ -1,5 +1,53 @@
 package hexlet.courses.lesson6.exercises;
 
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.Arrays;
+
 public class Exercises {
 
+    public static int[][] rotateLeft(int[][] matrix) {
+        var rowsCount = matrix.length;
+        var columnsCount = matrix[0].length;
+        var rotatedMatrix = new int[columnsCount][rowsCount];
+
+        for (var row = 0; row < columnsCount; row++) {
+
+            rotatedMatrix[row] = new int[rowsCount];
+
+            for (var coll = 0; coll < rowsCount; coll++) {
+                rotatedMatrix[row][coll] =  matrix[coll][columnsCount - row - 1];
+            }
+        }
+
+        return rotatedMatrix;
+    }
+
+    public static int[] getNumbersFromArray(int[][] array, int deep, int rotateCount) {
+        var arrayToCopy = array[deep];
+        var copyTo = array.length == 1 ? arrayToCopy.length : arrayToCopy.length - 1;
+
+        int[] result = Arrays.copyOfRange(arrayToCopy, 0, copyTo);
+
+        var nextDeep = rotateCount > 0 && rotateCount % 4 == 0 ? deep + 1 : deep;
+
+        if (nextDeep >= array.length / 2) {
+            return array.length > 2 ? Arrays.copyOfRange(result, 1, result.length) : result;
+        }
+
+        var nextRotateCount = rotateCount + 1;
+        int[][] rotatedArray = rotateLeft(array);
+        var nextNumbers = getNumbersFromArray(rotatedArray, nextDeep, nextRotateCount);
+        var resultArray = ArrayUtils.addAll(result, nextNumbers);
+
+        return resultArray;
+    }
+
+    public static int[] buildSnailPath(int[][] array) {
+        if (array.length == 0) {
+            return new int[0];
+        }
+
+        return getNumbersFromArray(array, 0, 1);
+    }
 }
--- app/src/test/java/hexlet/courses/lesson6/exercises/ExercisesTest.java
+++ app/src/test/java/hexlet/courses/lesson6/exercises/ExercisesTest.java
@@ -1,5 +1,66 @@
 package hexlet.courses.lesson6.exercises;
 
-public class ExercisesTest {
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class ExercisesTest {
+    @Test
+    public void testBuildSnailPath1() {
+        int[][] matrix = {
+                {1, 2},
+                {3, 4}
+        };
+        int[] expected = {1, 2, 4, 3};
+        var actual = Exercises.buildSnailPath(matrix);
+        assertThat(actual).containsExactly(expected);
+    }
+
+    @Test
+    public void testBuildSnailPath2() {
+        int[][] matrix = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}
+        };
+        int[] expected = {1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7};
+        var actual = Exercises.buildSnailPath(matrix);
+        assertThat(actual).containsExactly(expected);
+    }
+
+    @Test
+    public void testBuildSnailPath3() {
+        int[][] matrix = {};
+        int[] expected = {};
+        var actual = Exercises.buildSnailPath(matrix);
+        assertThat(actual).containsExactly(expected);
+    }
+
+    @Test
+    public void testBuildSnailPath4() {
+        int[][] matrix = {{1, 2, 3, 4}};
+        int[] expected = {1, 2, 3, 4};
+        var actual = Exercises.buildSnailPath(matrix);
+        assertThat(actual).containsExactly(expected);
+    }
+
+    @Test
+    public void testBuildSnailPath5() {
+        int[][] matrix = {{1}, {2}, {3}, {4}};
+        int[] expected = {1, 2, 3, 4};
+        var actual = Exercises.buildSnailPath(matrix);
+        assertThat(actual).containsExactly(expected);
+    }
+
+    @Test
+    public void testBuildSnailPath6() {
+        int[][] matrix = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12}
+        };
+        int[] expected = {1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7};
+        var actual = Exercises.buildSnailPath(matrix);
+        assertThat(actual).containsExactly(expected);
+    }
 }
